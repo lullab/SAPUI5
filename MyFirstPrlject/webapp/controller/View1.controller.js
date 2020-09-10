3213502b7e4a4b057caf270342cc520860acb491
lullab <lullab0404@gmail.com>
2020-09-10 12:19:00 +0000

    a

---
+++ MyFirstPrlject/webapp/controller/View1.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("com.logaligroup.MyFirstPrlject.controller.View1", {
+		onInit: function () {
+
+		}
+	});
+});
